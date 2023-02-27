dd304fdab007321a17353a31a33a4f9bbe209752
Salman Mahmud <salman_mahmud@hotmail.com>
2023-02-27 17:25:33 -0500

    colors and config/customization

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,13 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div>
+      <h1 className="border-4 border-green-600 text-xs sm:text-base md:text-lg lg:text-3xl mb-1" >
+        Hello world!
+      </h1>
+      <h1 className="text-vuejs">
+        Hello world!
+      </h1>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,45 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+
+    //can be used for things like margin
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
+
+
+    // colors: {
+    //   vuejs: "#41f07e"
+    // }, if you put this colors object it will replace all the built in tailwind colors with ones that you put. If you add them to extend it wont replace
+    extend: {
+
+      colors: {
+        vuejs: "#41f07e"
+      }
+
+    },
   },
+  // plugins are javascript helpers to create stylesheets using js
   plugins: [],
 }
